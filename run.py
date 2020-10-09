363900c7e8823868d5a3402a1f36e3e4d5f39424
Melvyn de Kort <m.de.kort@portbase.com>
2020-10-09 11:58:28 +0200

    Several improvements

--- run.py
+++ run.py
@@ -2,48 +2,25 @@
 
 import os
 import schedule
+import threading
 import time
+import functools
 import docker
+from pprint import pprint
 
 
-def resetPermissions():
-    print('Starting resetting filesystem permissions')
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    client.containers.run(image='melvyndekort/resetperms:1.2',
-                          auto_remove=True,
-                          detach=False,
-                          environment=[
-                            'SETUID=8888',
-                            'SETGID=8888',
-                            'DIRS=/host/data01/kids;/host/data01/movies;/host/data01/music;/host/data01/software;/host/data01/torrents;/host/data01/tv;/host/data01/usenet;/host/data01/xxx;/host/safe01/photos'
-                          ],
-                          name='resetperms',
-                          volumes={'/safe01': {'bind': '/host/safe01', 'mode': 'rw'},
-                                   '/data01': {'bind': '/host/data01', 'mode': 'rw'}})
-    print('Finished resetting filesystem permissions')
-    client.close()
+def with_logging(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('Running job "%s"' % func.__name__)
+        result = func(*args, **kwargs)
+        print('Job "%s" completed' % func.__name__)
+        return result
+    return wrapper
 
 
-def dyndns():
-    print('Starting dyndns update')
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    client.containers.run(image='melvyndekort/route53-dyndns:1.1',
-                          auto_remove=True,
-                          detach=False,
-                          environment=[
-                            'ZONEID=' + os.environ['ZONEID'],
-                            'FQDN=' + os.environ['FQDN'],
-                            'AWS_ACCESS_KEY_ID=' + os.environ['AWS_ACCESS_KEY_ID'],
-                            'AWS_SECRET_ACCESS_KEY=' + os.environ['AWS_SECRET_ACCESS_KEY']
-                          ],
-                          name='dyndns')
-    print('Finished dyndns update')
-    client.close()
-
-
-def backups():
-    print('Starting sonarr backup')
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+@with_logging
+def backupSonarr(client):
     client.containers.run(image='alpine:3.12',
                           auto_remove=True,
                           command='-O sonarr.zip http://sonarr:8989/api/system/backup?apikey=' + os.environ['SONARR_TOKEN'],
@@ -54,9 +31,10 @@ def backups():
                           volumes={'/safe01/backups/lmserver': {'bind': '/host/lmserver', 'mode': 'rw'}},
                           user=8888,
                           working_dir='/host/lmserver')
-    print('Finished sonarr backup')
 
-    print('Starting radarr backup')
+
+@with_logging
+def backupRadarr(client):
     client.containers.run(image='alpine:3.12',
                           auto_remove=True,
                           command='-O radarr.zip http://radarr:7878/api/system/backup?apikey=' + os.environ['RADARR_TOKEN'],
@@ -67,9 +45,10 @@ def backups():
                           volumes={'/safe01/backups/lmserver': {'bind': '/host/lmserver', 'mode': 'rw'}},
                           user=8888,
                           working_dir='/host/lmserver')
-    print('Finished radarr backup')
 
-    print('Starting restic backup to Wasabi: lmserver')
+
+@with_logging
+def backupLMServer(client):
     client.containers.run(image='restic/restic:0.9.6',
                           auto_remove=True,
                           command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/lmserver backup -H lmserver /host/backups',
@@ -81,9 +60,24 @@ def backups():
                           ],
                           name='restic',
                           volumes={'/safe01/backups': {'bind': '/host/backups', 'mode': 'ro'}})
-    print('Finished restic backup to Wasabi: lmserver')
 
-    print('Starting restic backup to Wasabi: syncthing')
+
+@with_logging
+def cleanupLMServer(client):
+    client.containers.run(image='restic/restic:0.9.6',
+                          auto_remove=True,
+                          command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/lmserver forget --prune --keep-last 21',
+                          detach=False,
+                          environment=[
+                            'AWS_ACCESS_KEY_ID=' + os.environ['WASABI_ACCESS_KEY_ID'],
+                            'AWS_SECRET_ACCESS_KEY=' + os.environ['WASABI_SECRET_ACCESS_KEY'],
+                            'RESTIC_PASSWORD=' + os.environ['RESTIC_PASSWORD']
+                          ],
+                          name='restic')
+
+
+@with_logging
+def backupSyncthing(client):
     client.containers.run(image='restic/restic:0.9.6',
                           auto_remove=True,
                           command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/syncthing backup -H lmserver /host/syncthing',
@@ -95,9 +89,24 @@ def backups():
                           ],
                           name='restic',
                           volumes={'/home/melvyn/Sync': {'bind': '/host/syncthing', 'mode': 'ro'}})
-    print('Finished restic backup to Wasabi: syncthing')
 
-    print('Starting restic backup to Wasabi: libvirt')
+
+@with_logging
+def cleanupSyncthing(client):
+    client.containers.run(image='restic/restic:0.9.6',
+                          auto_remove=True,
+                          command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/syncthing forget --prune --keep-last 21',
+                          detach=False,
+                          environment=[
+                            'AWS_ACCESS_KEY_ID=' + os.environ['WASABI_ACCESS_KEY_ID'],
+                            'AWS_SECRET_ACCESS_KEY=' + os.environ['WASABI_SECRET_ACCESS_KEY'],
+                            'RESTIC_PASSWORD=' + os.environ['RESTIC_PASSWORD']
+                          ],
+                          name='restic')
+
+
+@with_logging
+def backupLibvirt(client):
     client.containers.run(image='restic/restic:0.9.6',
                           auto_remove=True,
                           command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/libvirt backup -H lmserver /host/libvirt',
@@ -109,35 +118,10 @@ def backups():
                           ],
                           name='restic',
                           volumes={'/safe01/libvirt': {'bind': '/host/libvirt', 'mode': 'ro'}})
-    print('Finished restic backup to Wasabi: libvirt')
 
-    print('Starting cleaning of restic backups from Wasabi: lmserver')
-    client.containers.run(image='restic/restic:0.9.6',
-                          auto_remove=True,
-                          command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/lmserver forget --prune --keep-last 21',
-                          detach=False,
-                          environment=[
-                            'AWS_ACCESS_KEY_ID=' + os.environ['WASABI_ACCESS_KEY_ID'],
-                            'AWS_SECRET_ACCESS_KEY=' + os.environ['WASABI_SECRET_ACCESS_KEY'],
-                            'RESTIC_PASSWORD=' + os.environ['RESTIC_PASSWORD']
-                          ],
-                          name='restic')
-    print('Finished cleaning of restic backups from Wasabi: lmserver')
 
-    print('Starting cleaning of restic backups from Wasabi: syncthing')
-    client.containers.run(image='restic/restic:0.9.6',
-                          auto_remove=True,
-                          command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/syncthing forget --prune --keep-last 21',
-                          detach=False,
-                          environment=[
-                            'AWS_ACCESS_KEY_ID=' + os.environ['WASABI_ACCESS_KEY_ID'],
-                            'AWS_SECRET_ACCESS_KEY=' + os.environ['WASABI_SECRET_ACCESS_KEY'],
-                            'RESTIC_PASSWORD=' + os.environ['RESTIC_PASSWORD']
-                          ],
-                          name='restic')
-    print('Finished cleaning of restic backups from Wasabi: syncthing')
-
-    print('Starting cleaning of restic backups from Wasabi: libvirt')
+@with_logging
+def cleanupLibvirt(client):
     client.containers.run(image='restic/restic:0.9.6',
                           auto_remove=True,
                           command='--no-cache -r s3:https://s3.wasabisys.com/mdekort-backup/libvirt forget --prune --keep-last 21',
@@ -148,9 +132,10 @@ def backups():
                             'RESTIC_PASSWORD=' + os.environ['RESTIC_PASSWORD']
                           ],
                           name='restic')
-    print('Finished cleaning of restic backups from Wasabi: libvirt')
 
-    print('Starting rclone backup to StackStorage')
+
+@with_logging
+def backupPhotos(client):
     client.containers.run(image='rclone/rclone:1.53',
                           auto_remove=True,
                           command='/bin/sh -c "rclone config create stackstorage webdav; rclone sync /data stackstorage:photos --create-empty-src-dirs"',
@@ -164,18 +149,100 @@ def backups():
                           ],
                           name='rclone',
                           volumes={'/safe01/photos': {'bind': '/data', 'mode': 'ro'}})
-    print('Finished rclone backup to StackStorage')
+
+
+@with_logging
+def job_resetPermissions():
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    client.containers.run(image='melvyndekort/resetperms:1.2',
+                          auto_remove=True,
+                          detach=False,
+                          environment=[
+                            'SETUID=8888',
+                            'SETGID=8888',
+                            'DIRS=/host/data01/kids;/host/data01/movies;/host/data01/music;/host/data01/software;/host/data01/torrents;/host/data01/tv;/host/data01/usenet;/host/data01/xxx;/host/safe01/photos'
+                          ],
+                          name='resetperms',
+                          volumes={'/safe01': {'bind': '/host/safe01', 'mode': 'rw'},
+                                   '/data01': {'bind': '/host/data01', 'mode': 'rw'}})
     client.close()
 
 
+@with_logging
+def job_dyndns():
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    client.containers.run(image='melvyndekort/route53-dyndns:1.1',
+                          auto_remove=True,
+                          detach=False,
+                          environment=[
+                            'ZONEID=' + os.environ['ZONEID'],
+                            'FQDN=' + os.environ['FQDN'],
+                            'AWS_ACCESS_KEY_ID=' + os.environ['AWS_ACCESS_KEY_ID'],
+                            'AWS_SECRET_ACCESS_KEY=' + os.environ['AWS_SECRET_ACCESS_KEY']
+                          ],
+                          name='dyndns')
+    client.close()
+
+
+def waitUntilContainerStops(client, name):
+    counter = 0
+    while True:
+        if (counter >= 30):
+            raise Exception(f"Container {name} was running longer than expected")
+
+        try:
+            client.get(name)
+            counter += 1
+            time.sleep(1)
+        except docker.errors.NotFound:
+            break
+
+
+@with_logging
+def job_backups():
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+    backupSonarr()
+    waitUntilContainerStops(client, 'sonarr-backup')
+    backupRadarr()
+    waitUntilContainerStops(client, 'radarr-backup')
+
+    backupLMServer()
+    waitUntilContainerStops(client, 'restic')
+    cleanupLMServer()
+    waitUntilContainerStops(client, 'restic')
+
+    backupSyncthing()
+    waitUntilContainerStops(client, 'restic')
+    cleanupSyncthing()
+    waitUntilContainerStops(client, 'restic')
+
+    backupLibvirt()
+    waitUntilContainerStops(client, 'restic')
+    cleanupLibvirt()
+    waitUntilContainerStops(client, 'restic')
+
+    backupPhotos()
+
+    client.close()
+
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+
 def main():
-    schedule.every(1).hours.do(resetPermissions)
-    schedule.every(2).hours.do(dyndns)
-    schedule.every().day.at("02:00").do(backups)
+    schedule.every(1).hours.do(run_threaded, job_resetPermissions)
+    schedule.every(2).hours.do(run_threaded, job_dyndns)
+    schedule.every().day.at("02:00").do(run_threaded, job_backups)
+
+    print('Loaded jobs:')
+    pprint(schedule.jobs)
     
     while 1:
-      schedule.run_pending()
-      time.sleep(1)
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__": 
