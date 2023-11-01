27cd024397d7cc202983232077deddcfa9316d44
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-01 16:45:41 +0100

    Make webroot configurable

--- trigger_web/main.py
+++ trigger_web/main.py
@@ -1,6 +1,7 @@
 import logging
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from logging.config import dictConfig
 
 from trigger_scheduler import main as scheduler
@@ -24,6 +25,15 @@ dictConfig({
 
 app = Flask(__name__)
 
+if 'WEBROOT' in os.environ:
+    webroot = os.environ['WEBROOT']
+else:
+    webroot = '/scheduler'
+
+
+@app.route('/')
+def root_get():
+    return redirect(webroot, code=302)
 
 def get_jobs():
     jobs = scheduler.get_docker_jobs()
@@ -35,17 +45,22 @@ def get_jobs():
 
     return jobs
 
-@app.route('/', methods=['GET'])
+@app.route(webroot, methods=['GET'])
 def index_get():
-    return render_template('index.html', docker_jobs=get_jobs())
+    return render_template(
+        'index.html',
+        webroot=webroot,
+        docker_jobs=get_jobs()
+    )
 
-@app.route('/', methods=['POST'])
+@app.route(webroot, methods=['POST'])
 def index_post():
     jobname = request.form.get('triggerJobName')
 
     if jobname is None:
         return render_template(
             'index.html',
+            webroot=webroot,
             error='No valid job was triggered',
             docker_jobs=get_jobs()
         )
@@ -55,12 +70,14 @@ def index_post():
     if result:
         return render_template(
             'index.html',
+            webroot=webroot,
             trigger=f'Job "{jobname}" was successfully triggered',
             docker_jobs=get_jobs()
         )
     else:
         return render_template(
             'index.html',
+            webroot=webroot,
             error=f'Job "{jobname}" could not be triggered',
             docker_jobs=get_jobs()
         )
