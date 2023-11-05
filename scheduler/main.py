0bffdb4e4187ba55a79a70b97134d869d6668ce0
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-05 15:07:50 +0100

    Use APScheduler for the actual scheduling, since it supports crontab strings

--- scheduler/main.py
+++ scheduler/main.py
@@ -1,15 +1,41 @@
 import logging
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+
 FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
 
-from scheduler import config, docker, schedule
-jobs = config.get_jobs()
+from scheduler import config, docker
 
+jobs = config.get_jobs()
+scheduler = BlockingScheduler()
+
+
+def run_job(job):
+    logger.info(f'Triggering {job.name}')
+
+#    if job.jobtype == 'exec':
+#        logger.info(f'Triggering {job.name}')
+#        docker.start_exec(job)
+#    elif job.jobtype == 'run':
+#        docker.start_run(job)
+#    else:
+#        raise NotImplemented
 
 def main():
-    schedule.start()
+    for job in jobs:
+        scheduler.add_job(
+            func=run_job,
+            trigger=CronTrigger.from_crontab(job.schedule),
+            id=job.name,
+            name=job.name,
+            args=[job]
+        )
+
+    scheduler.start()
+
 
 if __name__ == "__main__":
     main()
